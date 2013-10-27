81a7303fe12e89963d8d83a609a07e13b3b2cd41
Rémi Berson <remi.berson@gmail.com>
2013-10-28 00:54:34 +0100

    added simulated annealing and extract function

--- src/LineExtraction.jl
+++ src/LineExtraction.jl
@@ -1,5 +1,12 @@
 module LineExtraction
 
-# package code goes here
+using Color
+using Images
+using ImageView
+
+include("simulated_annealing.jl")
+include("extraction.jl")
+
+export extract
 
 end # module
