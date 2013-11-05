95d41af2ca071d6312c555bb46ae81df0c17f250
Vincent Latrouite <vincentlat.bloody@gmail.com>
2013-11-05 20:26:09 +0100

    new version with good matches, need to make the imwrite working.

--- src/extraction.jl
+++ src/extraction.jl
@@ -14,11 +14,13 @@ function mod_fun(row_map, arr, col, scores)
     end
     elts = row_map[j]               # We get the elements on j-th column.
     nb_elt_on_col = size(elts)[1]   # We get the number of elements.
-    start_index = arr[j, 3]
+    start_index = arr[j, 3]         # We get the nearest index from the start point.
+    # Randomly selecting a white pixel according to the normal distribution.
     rnd = randn()
-    index = int(rnd * (10) + start_index)
+    index = int(rnd * (nb_elt_on_col / 2) + start_index)
     index = max(index, 1)
     index = min(index, nb_elt_on_col)
+
     #index = rand(1:nb_elt_on_col)
     i = elts[index]
 
@@ -39,22 +41,19 @@ function cost_fun(orig, new, arr, pos, start)
     r, c = pos
 
     alpha = 1
-    #beta = 12
-    beta = 40
+    beta = 20
 
     cost = 0
-    #cost += (abs(r - row) / 40) ^ 5
-    #if  c > 1 && arr[c - 1, 1] != arr[c, 1]
-    #    cost += abs(arr[c - 1, 1] - arr[c, 1])
-    #end
+    # We score the distance between the start point and the pixel.
     if c > 1 && arr[c - 1, 1] == -1
         cost += alpha * abs(r - row)
     end
+    # We score the distance between the previous marked pixel and the current
+    # one, weighted with the square of root of the distance with the start
+    # pixel. We want the relation between neighbors to be important, but we
+    # also want to be close to the line to match.
     if c > 1 && arr[c - 1, 1] != -1
         cost += beta * abs(r - arr[c - 1, 1]) * (abs(r - row) + 1) ^ (1/2)
-    end
-    if c < size(arr,1) && arr[c + 1, 1] != -1
-        cost += beta * abs(r - arr[c + 1, 1]) * (abs(r - row) + 1) ^ (1/2)
     end
 
     cost
@@ -67,37 +66,44 @@ function extract(file_path, row, col, out_path)
 
     # Read input image to sparse matrix
     img = convert(Array, float64(imread(file_path)))
-    img[row, col] = 255
+    img[row, col] = 255                       # In order to be sure there is at least one pixel on this column.
     orig = sparse(img)
+
+    # Get number of rows and columns of the image.
     rows, cols = size(orig)
 
     # Candidate
     I, J, V = findnz(orig)
-    arr = zeros(cols, 3)
-    row_map = map((x) -> I[J .== x], 1:cols)
+    arr = zeros(cols, 3)                      # arr contains the tuples (row, index, start_index) marked, one by column.
+    row_map = map((x) -> I[J .== x], 1:cols)  # row_map contains the indexes of the white pixels.
 
     # Initial random candidate
-    nbelem = size(V, 1)
-    V *= 0
     for j = 1:cols
-        elts = row_map[j]                    # We get elements on j-th column.
-        nb_elt_on_col = size(elts)[1]   # We get the number of elements.
+        elts = row_map[j]                     # We get elements on j-th column.
+        nb_elt_on_col = size(elts)[1]         # We get the number of elements.
+
         start_index = 0
+        # If there isn't any white pixel on the column, we don't mark it.
         if (nb_elt_on_col == 0)
             i = -1
             index = -1
         else
+            # We initialize with the nearest (row speaking) pixel from the
+            # start pixel.
             start_index = max(size(row_map[j][row_map[j] .<= row], 1), 1)
-            #rnd = randn()
-            #index = int(rnd * (10) + start_index)
-            #index = max(index, 1)
-            #index = min(index, nb_elt_on_col)
-            #println("start_index: $(start_index + 0), nb_elt: $nb_elt_on_col rnd: $rnd, index: $index")
-            index = start_index
-            i = elts[index]       # We randomly pick a pixel on this column.
+            # Then we mark a random one, according to the normal distribution.
+            rnd = randn()
+            index = int(rnd * (nb_elt_on_col / 2) + start_index)
+            index = max(index, 1)
+            index = min(index, nb_elt_on_col)
+
+            #index = start_index
+            i = elts[index]
         end
-        arr[j,:] = [i, index, start_index]                      # We mark this pixel.
+        arr[j,:] = [i, index, start_index]    # We mark this pixel.
     end
+    # Set start point.
+    arr[col, 1] = row
 
     obj = sparse(I, J, V)
 
@@ -108,8 +114,8 @@ function extract(file_path, row, col, out_path)
 
     # Simulated annealing settings
     t = 2500.0
-    t_step = 0.99999
-    t_stop = 1
+    t_step = 0.999999
+    t_stop = 5
     max_epoc = 10000000000
     epoc = 0
 
@@ -175,5 +181,5 @@ function extract(file_path, row, col, out_path)
     # Display result
     img2 = convert(Image, img)
     display(img2)
-    #imwrite(img2, out_path)
+    imwrite(img2, out_path)
 end
